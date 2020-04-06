ff05c9e95d9aa98f5a0736ef7f3ef20523373858
Sai-Krishna-Web <60420794+Sai-Krishna-Web@users.noreply.github.com>
2020-04-06 15:43:13 +0530

    Update TodoPrivateWrapper.js

--- src/components/Todo/TodoPrivateWrapper.js
+++ src/components/Todo/TodoPrivateWrapper.js
@@ -6,7 +6,7 @@ import TodoPrivateList from "./TodoPrivateList";
 const TodoPrivateWrapper = () => {
   return (
     <div className="todoWrapper">
-      <div className="sectionHeader">Personal todos</div>
+      <div className="sectionHeader">Personal Tasks</div>
 
       <TodoInput />
       <TodoPrivateList />
