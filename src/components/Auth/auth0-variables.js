864faedbd3ac98a336a536ee4d4c25288775e7fe
Sai-Krishna-Web <60420794+Sai-Krishna-Web@users.noreply.github.com>
2020-04-06 15:35:23 +0530

    Add files via upload

---
+++ src/components/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'dev-3de3blfg.auth0.com',
+  clientId: 'w5H5SEXcIuH8P22Brp12fdwLSeCDLP3n',
+  callbackUrl: 'http://localhost:3000/callback'
+};
---
+++ src/components/OnlineUsers/OnlineUsersWrapper.js
@@ -0,0 +1,71 @@
+import React, { useEffect,Fragment, useState } from "react";
+ import { useMutation, useSubscription } from "@apollo/react-hooks";
+ import gql from "graphql-tag";
+
+import OnlineUser from "./OnlineUser";
+
+const OnlineUsersWrapper = () => {
+  const [onlineIndicator, setOnlineIndicator]= useState(0);
+  let onlineUsersList;
+  useEffect(()=>{
+    updateLastSeen();
+    setOnlineIndicator(setInterval(()=> updateLastSeen(), 30000
+    ));
+    return ()=>{ 
+      clearInterval(onlineIndicator);
+    };
+  },[]);
+
+  const UPDATE_LASTSEEN_MUTATION= gql`
+  mutation updateLastSeen($now:timestamptz!){
+    update_users(where:{},  _set:{ last_seen:$now}){
+      affected_rows
+    }
+  }
+  `;
+
+  const [updateLastSeenMutation]= useMutation(UPDATE_LASTSEEN_MUTATION);
+  const updateLastSeen=()=>{
+    updateLastSeenMutation({
+      variables:{now: new Date().toISOString()}
+    });
+  };
+  const {loading, error, data}=useSubscription(
+    gql`
+        subscription getOnlineUsers {
+          online_users(order_by:{user:{name:asc}}){
+            id
+            user{
+              name
+            }
+          }
+        }
+    
+    `
+  );
+  if(loading){
+    return <span>Loading...</span>;
+
+  }
+  if(error){
+    console.error(error);
+    return<span>Error!</span>;
+  }
+  if(data){
+    onlineUsersList=data.online_users.map(u=>(
+      <OnlineUser key={u.id} user={u.user} />
+    ));
+  }
+  return (
+    <div className="onlineUsersWrapper">
+       <Fragment>
+         <div className="sliderHeader">
+           Online users - {onlineUsersList.length}
+         </div>
+         {onlineUsersList}
+       </Fragment>
+     </div>
+  );
+};
+
+export default OnlineUsersWrapper;
